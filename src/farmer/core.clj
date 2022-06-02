a2ff287939404fb242532cab2ff5be9b054bb539
Brendan Foote <brendan@bigfoote.io>
2022-06-01 22:12:00 -0500

    Refactoring with the idea of storing the side the farmer is on rather than have the farmer be an element of the set on each side in order to reduce calculation

--- src/farmer/core.clj
+++ src/farmer/core.clj
@@ -2,38 +2,44 @@
   (:gen-class)
   (:require [clojure.set :as set]))
 
-(def start-state {:left #{:f :c :d :g} :right #{}})
+(def start-state {:left #{:c :d :g} :right #{} :farmer-side :left})
+
+(defn other-side [state]
+  (if (= (:farmer-side state) :left) :right :left))
 
 (defn search
   [state path history]
   (println "state: " state "path: " path "history: " history)
-  (cond (= state {:left #{} :right #{:f :c :d :g}}) path
-        (some identity (map #(= state %) history)) (do (println "already been here") false)
-        (fail? state) (do (println "no good!") false)
+  (cond (= state {:left #{} :right #{:c :d :g} :farmer-side :right}) path
+        (state history) (do (println "already been here") false)
+        (bad? state) (do (println "no good!") false)
         :else (try-moves (gen-moves state) state path history)))
 
 (defn try-moves
-  [moves state path history]
+  [moves state farmer-side path history]
   (if (not (seq moves))
     false
-    (or (search (next-state (first moves) state) (conj path (first moves)) (conj history state))
-        (try-moves (rest moves) state path history))))
+    (or (search (next-state (first moves) state)
+                (other-side state)
+                (conj path (first moves))
+                (conj history state))
+        (try-moves (rest moves) state farmer-side path history))))
 
-(defn fail?
+(defn bad?
   [state]
-  (let [farmerless-group (if (:f (:right state)) (:left state) (:right state))]
-    (and (:c farmerless-group) (or (:g farmerless-group) (:d farmerless-group)))))
+  (let [other-group (state (other-side (:farmer-side state)))]
+    (and (:c other-group) (or (:g other-group) (:d other-group)))))
 
 (defn gen-moves
   [state]
-  (let [farmer-group (if (:f (:right state)) (:right state) (:left state))]
-    (map (fn [x] (set/union #{:f} #{x})) farmer-group)))
+  (let [farmer-group ((:farmer-side state) state)]
+    (conj (map #(set (list %)) farmer-group) #{})))
 
 (defn next-state
   [move state]
-  (let [farmer-side (if (:f (:right state)) :right :left)
-        other-side (if (:f (:right state)) :left :right)]
-    {farmer-side (apply disj (farmer-side state) move) other-side (set/union (other-side state) move)}))
+  {(:farmer-side state) (apply disj (:farmer-side state) move)
+   (other-side state) (set/union (other-side (:farmer-side state)) move)
+   :farmer-side (other-side (:farmer-side state))})
 
 (defn print-path
   ([path] path)
@@ -42,7 +48,7 @@
      (println "Carry " (second p) " across"))))
 
 (defn solve []
-  (print-path (search start-state [] [])))
+  (print-path (search start-state :left [] #{})))
 
 (defn -main
   "I don't do a whole lot ... yet."
